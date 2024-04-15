3251ed1eebdf4405ef7d1a22727a692344f94808
polyresilient <97691869+polyresilient@users.noreply.github.com>
2024-04-15 15:13:55 +0200

    changed to tab from table

--- main_Tv_analysis_hrs.R
+++ main_Tv_analysis_hrs.R
@@ -3,10 +3,10 @@
 library(tidyverse)
 library(here)
 
-tv_hours_table <- gss_cat %>%
+tv_hours_tab <- gss_cat %>%
  filter(age<30) %>%
   group_by(marital) %>%
   summarize(mean_tv_hours = mean(tvhours, na.rm = TRUE))
 
-write_csv(tv_hours_table, here("tv_hours_table.csv"))
+write_csv(tv_hours_tab, here("tv_hours_tab.csv"))
 
