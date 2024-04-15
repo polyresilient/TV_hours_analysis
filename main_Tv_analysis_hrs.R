bbd94f77f3b7274ec7ab3989565c81ce5f1e6001
polyresilient <emitchell@itg.be>
2024-04-15 15:15:38 +0200

    shortened

--- main_Tv_analysis_hrs.R
+++ main_Tv_analysis_hrs.R
@@ -3,10 +3,10 @@
 library(tidyverse)
 library(here)
 
-tv_hours_table <- gss_cat %>%
+tv_hours_tabulation <- gss_cat %>%
  filter(age<30) %>%
   group_by(marital) %>%
   summarize(mean_tv_hours = mean(tvhours, na.rm = TRUE))
 
-write_csv(tv_hours_table, here("tv_hours_table.csv"))
+write_csv(tv_hours_tabulation, here("tv_hours_tabulation.csv"))
 
